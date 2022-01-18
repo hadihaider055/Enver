ce02bd174a1a7ab4a56c6a4c95b81cbff9216c40
hadihaider055 <haiderhadi055@gmail.com>
2022-01-18 18:45:15 +0500

    Added portfolio section

---
+++ src/views/portfolio/data.js
@@ -0,0 +1,17 @@
+export const portfolioData = [
+  {
+    id: 1,
+    image: require("../../images/squid-game.png").default,
+    title: "Squid Game",
+  },
+  {
+    id: 2,
+    image: require("../../images/furniture.png").default,
+    title: "Furniture",
+  },
+  {
+    id: 3,
+    image: require("../../images/office.png").default,
+    title: "Office",
+  },
+];
--- src/views/portfolio/index.js
+++ src/views/portfolio/index.js
@@ -1,7 +1,12 @@
-import { StaticImage } from "gatsby-plugin-image";
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
 import * as styles from "./styles.module.scss";
 import Slider from "react-slick";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { portfolioData } from "./data";
 
 const Portfolio = () => {
   const settings = {
@@ -18,7 +23,7 @@ const Portfolio = () => {
       {
         breakpoint: 1200,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 3,
           slidesToScroll: 1,
           centerMode: false,
         },
@@ -29,32 +34,77 @@ const Portfolio = () => {
           slidesToShow: 1,
           slidesToScroll: 1,
           centerMode: false,
-          spaceBetween: 10,
+          spaceBetween: 5,
         },
       },
     ],
   };
   const slider = useRef(null);
+  const next = () => {
+    setclass("lostopacity");
+    setTimeout(() => {
+      setclass("fullopacity");
+    }, 1000);
+    slider.current.slickNext();
+  };
+  const previous = () => {
+    setclass("lostopacity");
+    setTimeout(() => {
+      setclass("fullopacity");
+    }, 1000);
+    slider.current.slickPrev();
+  };
+  const [currentclass, setclass] = useState("fullopacity");
 
   return (
     <div
       id="portfolio"
       className="mt-28 max-w-screen-xl w-full mx-auto px-6 relative"
     >
-      <div className="">
+      <div>
         <h2 className="font-nunito text-white text-5xl font-bold tracking-wide">
           Our Awesome Portofolio
         </h2>
-        <StaticImage
-          src={"../../images/circles.png"}
+        <img
+          src={require("../../images/circles.png").default}
           alt="Circles"
-          className="absolute top-20 right-5 md:right-28"
+          className="absolute top-16 right-5 md:right-28"
           width={50}
           height={50}
         />
         <span className={styles.herobgDown}></span>
       </div>
-      <Slider {...settings} ref={slider}></Slider>
+      <div className="w-full">
+        <div
+          className={`flex absolute ${styles.reviewsBtnLeft} ${currentclass} bg-secondary rounded-full px-5 py-3  text-3xl z-10 text-white items-center justify-center cursor-pointer`}
+          onClick={() => {
+            previous();
+          }}
+        >
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </div>
+        <div
+          className={`flex absolute right-0 ${styles.reviewsBtnRight} ${currentclass} bg-secondary rounded-full px-5 py-3 text-3xl z-10 text-white items-center justify-center cursor-pointer`}
+          onClick={() => {
+            next();
+          }}
+        >
+          <FontAwesomeIcon icon={faChevronRight} />
+        </div>
+        <div className="w-full">
+          <Slider {...settings} ref={slider}>
+            {portfolioData.map((item) => (
+              <div key={item.id} className="flex w-full justify-center py-2">
+                <img
+                  src={item.image}
+                  alt={item.title}
+                  className="w-full md:w-4/5 review-card p-5 md:p-20"
+                />
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
     </div>
   );
 };
